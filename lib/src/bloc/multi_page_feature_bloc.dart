ac3e6774bcf6a59c529999cfa4c061bf869d1169
Antonello Galipò <antonello.galipo@crispybacon.it>
2020-11-09 12:43:16 +0100

    dependencies update

--- lib/src/bloc/multi_page_feature_bloc.dart
+++ lib/src/bloc/multi_page_feature_bloc.dart
@@ -1,5 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 
 import 'multi_page_feature_event.dart';
